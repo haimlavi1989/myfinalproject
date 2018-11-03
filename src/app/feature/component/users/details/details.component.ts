22b5be84d8624e6e6269f1e3eac1e3763298a143
haim lavi <haimlavi1989@gmail.com>
2018-11-04 00:59:57 +0200

    fix params changes in url

--- src/app/feature/component/users/details/details.component.ts
+++ src/app/feature/component/users/details/details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 import { BackendapiService } from '../../../services/backendapi.service'
 import { Subscription } from 'rxjs';
 import { address } from '../../../../shared/address.module';
@@ -14,12 +14,13 @@ import { task } from '../../../../shared/task.module';
 })
 export class DetailsComponent implements OnInit, OnDestroy {
 
-  private httpSubscription: Subscription;
-  id: number = 0;
+  private paramsSubscription: Subscription;
+  id: number = -1;
   userData: user;
   address: address;
   update: boolean = false;
   delete: boolean = false;
+  userslength: number;
   postsData: post[]; 
   tasksData: task[];
 
@@ -31,22 +32,37 @@ export class DetailsComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.httpSubscription = this.ActivatedR.queryParamMap.subscribe( data => {
 
-      this.id = +this.ActivatedR.snapshot.params.id-1;
-
-      this.userData.name = this.service.users[this.id].name;
-      this.userData.email = this.service.users[this.id].email;
-      this.userData.address.city = this.service.users[this.id].address.city;
-
-      this.postsData = this.service.posts.filter( cell => cell.userId == this.id+1 );
-      this.tasksData = this.service.tasks.filter( cell => cell.userId == this.id+1 );
-
+    this.id = +this.ActivatedR.snapshot.params.id-1;
+    if (this.userData.name != "") {
+      this.init();
+    } else {
+      this.service.getUsers().subscribe( response => {
+        this.init();
+      });
+    }
+    
+    this.paramsSubscription = this.ActivatedR.params.subscribe( (params: Params) => {
+              this.id = +params['id']-1;
+              this.init();
     });
+
+  }
+
+  init() {
+      this.userslength = this.service.users.length;
+      if ( this.id >= 0 && this.id  <= this.userslength ) { 
+        this.userData.name = this.service.users[this.id].name;
+        this.userData.email = this.service.users[this.id].email;
+        this.userData.address.city = this.service.users[this.id].address.city;
+    
+        this.postsData = this.service.posts.filter( cell => cell.userId == this.id+1 );
+        this.tasksData = this.service.tasks.filter( cell => cell.userId == this.id+1 );
+      }
   }
 
   ngOnDestroy() {
-    this.httpSubscription.unsubscribe();
+    this.paramsSubscription.unsubscribe();
   }
 
   mainpage() {
